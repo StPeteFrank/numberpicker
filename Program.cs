822eb8586188e7923775f664d1442774a9c921a2
Frank Diaz <email-diazfrankp@gmail.com>
2018-12-10 13:41:22 -0500

    Initial commit. Started my first C# app.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace numberpicker
+{
+  class Program
+  {
+
+    static int PickNumber(int randomNumber)
+    {
+      var result = new Random().Next(randomNumber) + 1;
+      // Side effects
+      return result;
+    }
+
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Let's create a numberpicker game");
+
+      Console.WriteLine("Pick a number between 1 and 20");
+
+    }
+  }
+}
